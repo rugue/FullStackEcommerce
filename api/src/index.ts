24da35e1e003af165d2ce02615b16f1fd9ae289a
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-18 21:20:03 +0100

    feat: add product endpoints for listing, creating, and retrieving products

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,8 +8,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Products endpoints
 app.get("/products", (req, res) => {
   res.send("the list of product!");
+});
+
+app.post("/products", (req, res) => {
+  res.send("the product has been created!");
+});
+
+app.get("/products/:id", (req, res) => {
+  console.log(req.params);
+  res.send("one product!");
 });
 
 app.listen(port, () => {
