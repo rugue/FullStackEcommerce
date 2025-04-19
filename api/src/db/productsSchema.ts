d4c0d83aeaa162f7d4ce8695efa7d7bf77802dcd
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-19 22:09:41 +0100

    feat: add quantity column to products table and update related metadata

--- api/src/db/productsSchema.ts
+++ api/src/db/productsSchema.ts
@@ -12,4 +12,6 @@ export const productsTable = pgTable("products", {
   description: text(),
   image: varchar({ length: 255 }),
   price: doublePrecision().notNull(),
+
+  quantity: integer().default(0),
 });
