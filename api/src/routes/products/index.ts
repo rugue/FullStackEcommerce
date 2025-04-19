c85d8e1f42160ef217dc839891b0f49fadb093c8
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-19 01:23:48 +0100

    feat: consolidate product route handlers into a single post and get by ID implementation

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
-import { listProducts } from "./productsController";
+import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from "./productsController";
 
 // Products endpoints
 const router = Router();
+
 router.get("/", listProducts);
 
-router.post("/", (req, res) => {
-  res.send("the product has been created!");
-});
+router.post("/", createProduct);
 
-router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send("one product!");
-});
+router.get("/:id", getProductById);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 export default router;
