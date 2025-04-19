fe8bfc7c021829a6e87e2a1c52c9d547b31cefa5
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-19 01:38:12 +0100

    feat: add middleware for JSON and URL-encoded request parsing

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products/index";
 
 // const express = require("express");
 const app = express();
+app.use(urlencoded({ extended: false }));
+app.use(json());
 const port = 3000;
 
 app.get("/", (req, res) => {
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -10,6 +10,7 @@ export function getProductById(req: Request, res: Response) {
 }
 
 export function createProduct(req: Request, res: Response) {
+  console.log(req.body);
   res.send("createProduct");
 }
 
