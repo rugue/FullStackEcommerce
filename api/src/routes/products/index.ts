5554d071e36e71fe99b4e8fd1bad3f6b1d4a82ea
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-20 18:46:39 +0100

    feat: add validation middleware and update product creation endpoint to use schema validation

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,12 +1,31 @@
 import { Router } from "express";
-import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from "./productsController";
+import {
+  listProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./productsController";
+import { validateData } from "../../middlewares/validationMiddleware";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+import { productsTable } from "../../db/productsSchema";
+
+// const createProductSchema = z.object({
+//   name: z.string().min(1),
+//   description: z.string().min(1),
+//   price: z.number().positive(),
+//   imageUrl: z.string().url().optional(),
+// });
+
+const createProductSchema = createInsertSchema(productsTable);
 
 // Products endpoints
 const router = Router();
 
 router.get("/", listProducts);
 
-router.post("/", createProduct);
+router.post("/", validateData(createProductSchema), createProduct);
 
 router.get("/:id", getProductById);
 
