45670d09e1098e2fabbc977ae24cb7d5ec75194c
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-20 08:21:57 +0100

    fix: update database connection and refactor product creation logic

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { db } from "../../db";
+import { productsTable } from "../../db/productsSchema";
 
 export function listProducts(req: Request, res: Response) {
   res.send("listProducts");
@@ -9,9 +11,18 @@ export function getProductById(req: Request, res: Response) {
   res.send("getProductById");
 }
 
-export function createProduct(req: Request, res: Response) {
-  console.log(req.body);
-  res.send("createProduct");
+export async function createProduct(req: Request, res: Response) {
+  try {
+    const [product] = await db
+      .insert(productsTable)
+      .values(req.body)
+      .returning();
+    res.status(201).json(product);
+    
+  } catch (e) {
+    res.status(500).send(e);
+  }
+  
 }
 
 export function updateProduct(req: Request, res: Response) {
