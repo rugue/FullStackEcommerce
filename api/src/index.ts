dfa2fe24722e2a70d23a0f032714cc8d8d6ad8d0
Enehizena Osarugue <nehirugue@gmail.com>
2025-04-23 16:57:20 +0100

    feat: implement orders management with create, list, get, and update functionalities

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products/index.js";
 import authRoutes from "./routes/auth/index.js";
+import ordersRoutes from "./routes/orders/index.js";
+// import stripeRoutes from "./routes/stripe/index.js";
 
 // const express = require("express");
 const app = express();
@@ -14,6 +16,8 @@ app.get("/", (req, res) => {
 
 app.use("/products", productsRoutes);
 app.use("/auth", authRoutes);
+app.use("/orders", ordersRoutes);
+// app.use("/stripe", stripeRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
